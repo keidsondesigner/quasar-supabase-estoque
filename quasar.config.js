5d873f737677c9ba96a8492d2a0700990690113c
Keidson Roby <keidsondesigner@gmail.com>
2023-12-11 13:57:38 -0400

    style: :lipstick: Config paleta de cores do projeto

--- quasar.config.js
+++ quasar.config.js
@@ -91,7 +91,18 @@ module.exports = configure((/* ctx */) => ({
 
   // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
   framework: {
-    config: {},
+    config: {
+      brand: {
+        primary: '#2c9c6a',
+        secondary: '#26A69A',
+        accent: '#9C27B0',
+        dark: '#1d1d1d',
+        positive: '#21BA45',
+        negative: '#C10015',
+        info: '#31CCEC',
+        warning: '#F2C037',
+      },
+    },
 
     // iconSet: 'material-icons', // Quasar icon set
     // lang: 'en-US', // Quasar language pack
