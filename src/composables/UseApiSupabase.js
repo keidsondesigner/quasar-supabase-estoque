28440f6f29e4af1fef27f5f0eb17120ebdbd986c
keidson designer <keidsondesigner@gmail.com>
2024-01-13 16:01:15 -0400

    feat: :sparkles: Criado o Composable UseApiSupabase

---
+++ src/composables/UseApiSupabase.js
@@ -0,0 +1,70 @@
+import useSupabase from 'src/boot/supabase';
+import useAuthUser from './UseAuthUser';
+
+export default function UseApiSupabase() {
+  const supabase = useSupabase();
+  const { user } = useAuthUser();
+
+  const list = async (table) => {
+    const { data, error } = await supabase
+      .from(table)
+      .select('*');
+
+    if (error) throw error;
+    return data;
+  };
+
+  const getById = async (table, id) => {
+    const { data, error } = await supabase
+      .from(table)
+      .select('*')
+      .eq('id', id); // comparando se o campo id é igual ao campo passado;
+
+    if (error) throw error;
+    return data[0];
+  };
+
+  const post = async (table, form) => {
+    const { data, error } = await supabase
+      .from(table)
+      .insert([
+        {
+          ...form,
+          user_id: user.value.id,
+        },
+      ]);
+
+    if (error) throw error;
+    return data;
+  };
+
+  const update = async (table, form) => {
+    const { data, error } = await supabase
+      .from(table)
+      .update([
+        ...form,
+      ])
+      .match({ id: form.id });
+
+    if (error) throw error;
+    return data;
+  };
+
+  const remove = async (table, id) => {
+    const { data, error } = await supabase
+      .from(table)
+      .delete()
+      .match({ id });
+
+    if (error) throw error;
+    return data;
+  };
+
+  return {
+    list,
+    getById,
+    post,
+    update,
+    remove,
+  };
+}
